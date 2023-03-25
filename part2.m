3cefe7ff4c256d849e2055dbbe3ef1c26f300e8e
Piyush <piyushnags@gmail.com>
2023-03-25 14:44:22 -0400

    Modified K values in part2
    Changed K values for anisotropic diffusion in part2. Spokes and wheel images now get filtered but lose some edge quality. Slight increase in K for camera man image as well.

--- part2.m
+++ part2.m
@@ -3,7 +3,7 @@ img = imread('Images/cwheelnoise.gif');
 % Part a
 wheelplot(img, 0);
 for e = 1:100
-    img = anisoDiff(img, 10, 'exp');
+    img = anisoDiff(img, 50, 'exp');
     if (e == 5 || e == 20 || e == 100)
         wheelplot(img, e);
     end
@@ -14,14 +14,14 @@ img = imread('Images/cameraman.tif');
 
 imwrite(img, strcat("part2img/cameraman_img", num2str(0), ".png"))
 for e = 1:100
-    img = anisoDiff(img, 10, 'exp');
+    img = anisoDiff(img, 20, 'exp');
     if (e == 5 || e == 20 || e == 100)
         imwrite(img, strcat("part2img/cameraman_img_exp_", num2str(e), ".png"))
     end
 end
 
 for e = 1:100
-    img = anisoDiff(img, 10, 'inv');
+    img = anisoDiff(img, 20, 'inv');
     if (e == 5 || e == 20 || e == 100)
         imwrite(img, strcat("part2img/cameraman_img_inv_", num2str(e), ".png"))
     end
